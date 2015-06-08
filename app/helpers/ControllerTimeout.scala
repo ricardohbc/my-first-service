90e269d7eb5bcc2a21c1e234c3e2b231c36c93fd
Hugh Richardson <Hugh_Richardson@s5a.com>
2015-06-08 16:38:04 -0400

    oops missed one

--- app/helpers/ControllerTimeout.scala
+++ app/helpers/ControllerTimeout.scala
@@ -6,15 +6,25 @@ import scala.concurrent.duration._
 import scala.language.postfixOps
 
 trait ControllerTimeout extends ConfigHelper {
-  var actionTimeout = config getInt "controllers.timeout" millis
+  val actionTimeout = config getInt "controllers.timeout" //millis
 
-  def timeout[T](errorHandler: => T)(body: => T) : Future[T] = {
+  // If your code blocks, we'll just wrap it in a Future so we can time it out.  
+  // This won't make things magically async or scalable.
+  // If you really have long running synchronous code check your thread pool size at least.
+  // E.g https://www.playframework.com/documentation/2.4.x/ThreadPools
+  def syncTimeout[T](errorHandler: => T, time: Int = actionTimeout)(body: => T) : Future[T] =
+    timingoutFuture(errorHandler, time, Future(body))
+
+  // just call this if you already have a future from a nice async API, like reactivemongo  maybe.
+  // this method isn't exactly necessary but I kind of want to try and 
+  // force the user to pay some attention to blocking v actual non-blocking actions
+  def asyncTimeout[T](errorHandler: => T, time: Int = actionTimeout, f: Future[T]) : Future[T] =
+    timingoutFuture(errorHandler, time, f)
+
+  private def timingoutFuture[T](errorHandler: => T, time: Int, f: Future[T]): Future[T] = {
     val promise = Promise[T]()
-    val futureBody = scala.concurrent.Future {
-      body
-    }
-    val timeoutFuture = play.api.libs.concurrent.Promise.timeout(errorHandler, actionTimeout)
-    promise.tryCompleteWith(futureBody)
+    val timeoutFuture = play.api.libs.concurrent.Promise.timeout(errorHandler, time.millis)
+    promise.tryCompleteWith(f)
     promise.tryCompleteWith(timeoutFuture)
     promise future
   }

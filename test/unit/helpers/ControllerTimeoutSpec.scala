fc5dc73cde94faf5dcf5c5bd29e4f14cbfc221c6
Hugh Richardson <Hugh_Richardson@s5a.com>
2015-06-09 16:05:49 -0400

    tests

--- test/unit/helpers/ControllerTimeoutSpec.scala
+++ test/unit/helpers/ControllerTimeoutSpec.scala
@@ -1,5 +1,8 @@
 package unit.helpers
 
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec}
 import helpers.{ControllerPayload, ControllerTimeout}
 import play.api.test.{FakeApplication, FakeRequest}
@@ -24,9 +27,19 @@ with ControllerPayload {
   "ControllerTimeout" should {
     "return error on timeout" in {
       ((contentAsJson(
-        syncTimeout( onHandlerRequestTimeout( FakeRequest(GET, "/microservice-template")).as(JSON)) {
+        timeout( onHandlerRequestTimeout( FakeRequest(GET, "/microservice-template")).as(JSON)) {
           Thread.sleep(10000)
           Ok("Won't get here")
+        }
+      ) \ "errors").asInstanceOf[JsArray](0) \ "error").as[String] == "TimeoutException" shouldBe true
+    }
+    "return error on an async timeout" in {
+      ((contentAsJson(
+        withTimeout( onHandlerRequestTimeout( FakeRequest(GET, "/microservice-template")).as(JSON)) {
+          Future {
+            Thread.sleep(10000)
+            Ok("Won't get here")
+          }
         }
       ) \ "errors").asInstanceOf[JsArray](0) \ "error").as[String] == "TimeoutException" shouldBe true
     }

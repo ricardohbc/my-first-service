7e1397fc31ce48bb29a7d16c8d61f83853a4dbe1
mrsaks <mike_roth@s5a.com>
2015-06-19 11:19:35 -0400

    update api-doc version

--- project/CommonDependencies.scala
+++ project/CommonDependencies.scala
@@ -6,7 +6,7 @@ object CommonDependencies {
 
   val scalaTestVersion = "2.2.5"
   val scalaCheckVersion = "1.12.2"
-  val apiDocVersion = "8"
+  val apiDocVersion = "9"
 
   val scalaTest = "org.scalatest" %% "scalatest" % scalaTestVersion % "test"
   val apiDoc = "com.hbc" %% "api_doc" % apiDocVersion

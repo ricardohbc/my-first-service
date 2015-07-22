7e3e0821d473da4624ec9cadb460fc0f4a02baab
Hugh Richardson <Hugh_Richardson@s5a.com>
2015-07-22 15:25:32 -0400

    get rid of redundancy

--- app/helpers/ControllerTimeout.scala
+++ app/helpers/ControllerTimeout.scala
@@ -15,8 +15,7 @@ class TimingoutAction(ea: EssentialAction, timeout: Long = 1000L) extends Action
 
   override def apply(req: Request[AnyContent]): Future[Result] = {
     val goodResult: Future[Result] = ea.apply(req).run
-    val timeoutFuture = after(timeout millis, using = Akka.system.scheduler)(Future.failed(new TimeoutException(Constants.TIMEOUT_MSG)))
-    Future.firstCompletedOf(Seq(timeoutFuture, goodResult))(Contexts.ctx)
+    ControllerTimeoutLike.withTimeout(timeout)(goodResult)
   }
 }
 
@@ -24,15 +23,17 @@ trait ControllerTimeout extends ConfigHelper {
   val actionTimeout = config getInt "controllers.timeout"
 
   // call this with some arbitrary blocking code 
-  def timeout[T](time: Int = actionTimeout)(body: => T): Future[T] =
+  def timeout[T](time: Long = actionTimeout)(body: => T): Future[T] =
     timingoutFuture(time, Future(body))
 
   // call this if you already have a future
-  def withTimeout[T](time: Int = actionTimeout)(f: Future[T]): Future[T] =
+  def withTimeout[T](time: Long = actionTimeout)(f: Future[T]): Future[T] =
     timingoutFuture(time, f)
 
-  private def timingoutFuture[T](time: Int, f: Future[T]): Future[T] = {
+  private def timingoutFuture[T](time: Long, f: Future[T]): Future[T] = {
     val timeoutFuture = after(time millis, using = Akka.system.scheduler)(Future.failed(new TimeoutException(Constants.TIMEOUT_MSG)))
     Future.firstCompletedOf(Seq(f, timeoutFuture))(Contexts.ctx)
   }
 }
+
+object ControllerTimeoutLike extends ControllerTimeout

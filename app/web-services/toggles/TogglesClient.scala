933e439a17486c5aeb4a357b3144877a8ef3c4ae
Hugh Richardson <Hugh_Richardson@s5a.com>
2015-08-10 12:01:31 -0400

    test spray cache

--- app/web-services/toggles/TogglesClient.scala
+++ app/web-services/toggles/TogglesClient.scala
@@ -11,14 +11,26 @@ import spray.caching._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import models._
 
-object TogglesClient extends ConfigHelper {
+// no idea about ttl just yet. Do they change much? The service is pretty snappy so could be less aggressive with the cache if useful
+// the dev toggle server returns about 260 toggles total right now
+trait IndividualToggleCache {
+  val toggleCache: Cache[Toggle] = LruCache(maxCapacity = 500, initialCapacity = 275, timeToLive = Duration(24, "hours"))
+  def addToCache(toggle: Toggle) = toggleCache(toggle.toggle_name, () => Future.successful(toggle))
+}
+
+trait AllTogglesCache {
+  val allTogglesCache: Cache[Seq[Toggle]] = LruCache(maxCapacity = 1, initialCapacity = 1, timeToLive = Duration(24, "hours"))
+  def addToAllTogglesCache(key: String, toggles: Seq[Toggle]) = allTogglesCache(key, () => Future.successful(toggles))
+}
+
+object TogglesClient extends IndividualToggleCache with AllTogglesCache with ConfigHelper {
 
   private val svcUrl = config.getString("webservices.toggles.url")
 
   // no idea about ttl just yet. Do they change much? The service is pretty snappy so could be less aggressive with the cache if useful
   // the dev toggle server returns about 260 toggles total right now
-  private val toggleCache: Cache[Toggle] = LruCache(maxCapacity = 500, initialCapacity = 275, timeToLive = Duration(24, "hours"))
-  private val allTogglesCache: Cache[Seq[Toggle]] = LruCache(maxCapacity = 1, initialCapacity = 1, timeToLive = Duration(24, "hours"))
+  //private val toggleCache: Cache[Toggle] = LruCache(maxCapacity = 500, initialCapacity = 275, timeToLive = Duration(24, "hours"))
+  //private val allTogglesCache: Cache[Seq[Toggle]] = LruCache(maxCapacity = 1, initialCapacity = 1, timeToLive = Duration(24, "hours"))
   val unpackJsonResults: JsValue => JsValue = (json) => (json \ "response" \ "results")
 
   private def getFromToggleSvc[T](reqUrl: String)(handler: JsValue => T): Future[T] =
@@ -38,10 +50,7 @@ object TogglesClient extends ConfigHelper {
 
   private def getAllToggles(): Future[Seq[Toggle]] =
     getFromToggleSvc(svcUrl) { js => unpackJsonResults(js).as[Seq[Toggle]] }.map { toggles =>
-      // shove them in the individual toggle cache
-      toggles.foreach { toggle =>
-        toggleCache(toggle.toggle_name, { () => Future.successful(toggle) })
-      }
+      toggles.foreach(addToCache) // shove them in the individual toggle cache
       toggles.sortBy(_.toggle_name)
     }
 
@@ -55,7 +64,6 @@ object TogglesClient extends ConfigHelper {
   }
 
   // ************** Exposed Services ****************************
-
   def getToggle(name: String): Future[Toggle] = getCachedToggle(name)
 
   def getToggles(): Future[Seq[Toggle]] = getCachedToggles()

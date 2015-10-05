7c970f6c7ab7fdf812fcb42abe01bee13c104549
Fabio Cognigni <fabio.cognigni@gmail.com>
2015-10-05 20:32:01 +0100

    Removed unneeded docker build/tag/push scripts.
    These scripts shouldn't be used anymore with the Vagrant/Docker setup in our local dev env (they were meant to be used with boot2docker).
    Also, no one should need to push an image built from its local machine. It is Go responsibility of doing this and GO is not using these scripts for doing that.

--- docker/stop-service.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-echo "Stopping the hbc-microservice-template ..."
-docker stop hbc-microservice-template; docker rm hbc-microservice-template
-echo "... done."
--- docker/tag_and_push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Tag (override if necessary) the hbc-microservice-template..."
-sudo docker tag -f hbcdigital/service:hbc-microservice-template-0.1 hd1cutl01lx.saksdirect.com:5000/hbc-microservice-template-0.1
-
-echo "Push hbc-microservice-template image to Internal Docker Registry..."
-sudo docker push hd1cutl01lx.saksdirect.com:5000/hbc-microservice-template-0.1
-
-echo "Completed pushing hbc-microservice-template image to Internal Docker Registry."

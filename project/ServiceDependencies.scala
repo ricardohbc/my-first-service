e116c9e9984df4204b764e38b51ea1ce7c6b51e0
Himanshu Yadav <Himanshu_Yadav@s5a.com>
2016-01-04 12:07:32 -0500

    bump hornets-client

--- project/ServiceDependencies.scala
+++ project/ServiceDependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 /* List the dependencies specific to the service here */
 object ServiceDependencies {
 
-  val hornetqClientVersion = "78"
+  val hornetqClientVersion = "87"
   val hornetQClient = "com.hbc" %% "hornetq-client" % hornetqClientVersion
 
   val serviceDependencies : Seq[ModuleID] = Seq(hornetQClient)
